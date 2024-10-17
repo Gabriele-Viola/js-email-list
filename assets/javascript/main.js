5fa3c9167965e4155bec7703006850a2975fb5d3
Gabriele-Viola <gabrieleviola50@gmail.com>
2024-10-17 16:04:13 +0200

    reaname repo

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,8 +1,19 @@
+/*Descrizione
+Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+Bonus
+Abbellire con CSS o Bootstrap
+Inserire un bottone che al click fetchi altre 10 mail (sostituendo le altre)*/
 
 
-axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+let maniMails = 10
+for (let i = 0; i < maniMails; i++) {
+    const element = maniMails[i];
+    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
 .then(respo =>{
-    const result = respo.data;
-    console.log(result.response);
+    let result = respo.data;
     
+    
+    console.log(result.response);
 })
+    
+}
