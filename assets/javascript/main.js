14948a2a1429711b8af6f041790bd4a870cc1d8e
Gabriele-Viola <gabrieleviola50@gmail.com>
2024-10-17 16:36:54 +0200

    add p in html

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -6,14 +6,37 @@ Inserire un bottone che al click fetchi altre 10 mail (sostituendo le altre)*/
 
 
 let maniMails = 10
-for (let i = 0; i < maniMails; i++) {
-    const element = maniMails[i];
-    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-.then(respo =>{
-    let result = respo.data;
+
+const formEl= document.querySelector('form')
+console.log(formEl);
+const amountEl= document.getElementById('amount')
+console.log(amountEl);
+maniMails = amountEl.value
+console.log(maniMails);
+const listEl = document.querySelector('.list')
+console.log(listEl);
+
+
+
+formEl.addEventListener('submit', function (e) {
+    e.preventDefault()
+    listEl.innerHTML=''
+    maniMails = amountEl.value
+    amountEl.value=''
     
+    for (let i = 0; i < maniMails; i++) {
+        const element = maniMails[i];
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then(respo =>{
+        let result = respo.data;
+        console.log(result.response);
+        const email = document.createElement('p')
+        email.innerText = result.response
+        listEl.appendChild(email)
+    })
+        
+    }
     
-    console.log(result.response);
 })
-    
-}
+
+
