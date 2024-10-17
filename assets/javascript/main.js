71edce43ac36b2880ebd87d302bc3b17ec0b4d4c
Gabriele-Viola <gabrieleviola50@gmail.com>
2024-10-17 15:34:09 +0200

    add axios

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,2 +1,14 @@
 console.log('hi');
 axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+.them(respo => {
+    const result = response.data;
+    
+    
+})
+.catch(error => {
+    console.error(error)
+})
+.them(()=>{
+    console.log(respo);
+    
+})
